a3edcbc4d1af45e575e82fa890204d389c5c4860
Shinji Tsutsumi <tsutsumi@air.co.jp>
2024-11-26 13:28:35 +0000

    Copilot second commit3

--- member.js
+++ member.js
@@ -1,3 +1,11 @@
-function skillMember() {
-  return 'skillMember';
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        getSkills: function() {
+            return this.skills;
+        }
+    };
+    return member;
 }
